2e590abbd79ac9f5817aa5c067c548ce409eaaa8
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-24 18:13:11 -0500

    Build out 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,18 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
 
-const NotFoundPage = () => <h2>error page</h2>
+const NotFoundPage = () => (
+  <Layout>
+    <section className="error-page">
+      <div className="page-center">
+        <span>404</span>
+        <h3>Sorry, but these aren't the droids you're looking for</h3>
+        <Link to="/" className="btn">
+          Go back home young Skywalker
+        </Link>
+      </div>
+    </section>
+  </Layout>
+)
 
 export default NotFoundPage
