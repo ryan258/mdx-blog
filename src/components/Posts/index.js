00b45f006ecec199eee557166594a85c7ee64574
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-25 15:19:38 -0500

    Set up MDX converter, create posts block view, page view and post teaser display

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import Post from './Post'
 import Banner from '../Banner'
+
 const Posts = ({ posts, title }) => {
   return (
-    <h4>posts component</h4>
+    <section className="posts">
+      <h3 className="posts-title">{title}</h3>
+      <div className="posts-center">
+        <article>
+          {posts.map(post => (
+            <Post key={post.id} {...post} />
+          ))}
+        </article>
+        <article>
+          <Banner />
+        </article>
+      </div>
+    </section>
   )
 }
 
