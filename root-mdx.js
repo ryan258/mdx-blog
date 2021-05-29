4b5cebcf0c67e7c6a933eff4cfb9a2001129497a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-29 16:00:07 -0500

    Use MDX to super charge elements in mdx files

--- root-mdx.js
+++ root-mdx.js
@@ -1,8 +1,43 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
+import { Headings, Code, Blockquote } from './src/components/Complete'
+
+//! vv move this to own file - ./src/components/Complete
+// const myH2 = props => {
+//   // console.log(props)
+//   if (props.title) {
+//     return (
+//       <h2 {...props} style={{ fontSize: '4rem', color: 'red' }}>
+//         {props.children}
+//       </h2>
+//     )
+//   }
+//   return (
+//     <h2 {...props} className="code">
+//       {props.children}
+//     </h2>
+//   )
+// }
+
+const Testing = ({ children }) => {
+  return <div className="code">{children}</div>
+}
 
 const components = {
   // all the logic goes here
+  //! vv this below will make all h2's these spans vv
+  // h2: props => {
+  //   console.log(props)
+  //   return <h2 {...props}>{props.children}</h2>
+  // },
+  //! vv we'll seperate out the component into it's own fn above
+  // h2: myH2,
+  //! vv we'll access the component that now resides in a different file
+  h2: Headings.myH2,
+  h4: Headings.myH4,
+  // inlineCode: Testing,
+  inlineCode: Code,
+  blockquote: Blockquote,
 }
 
 // wrapRootElement must be the name
